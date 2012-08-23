61b3ac2a1f3cdd61a96910fe2e529e0ed0582ec0
Vladimir Tsvang <vtsvang@gmail.com>
2012-08-23 19:16:58 +0400

    new files structure

---
+++ grunt.js
@@ -0,0 +1,101 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // CoffeeScript plugin
+  grunt.loadNpmTasks('grunt-coffee');
+
+  // Clean plugin
+  grunt.loadNpmTasks('grunt-clean');
+
+  var task = grunt.task;
+  var registerTask = grunt.registerTask;
+  var file = grunt.file;
+  var utils = grunt.utils;
+  var log = grunt.log;
+  var verbose = grunt.verbose;
+  var fail = grunt.fail;
+  var option = grunt.option;
+  var config = grunt.config;
+  var template = grunt.template;
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+
+    clean: {
+      folder: 'app/js/'
+    },
+
+    meta: {
+      banner: [
+        '/* ',
+        ' <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ',
+        ' <%= grunt.template.today("yyyy-mm-dd") %>',
+        ' */'
+      ].join('\n')
+    },
+
+    coffee: {
+      app: {
+        src: ['app/coffee/*.coffee'],
+        dest: 'app/js/',
+        options: {
+            bare: false
+        }
+      },
+
+    tests: {
+        src: ['app/tests/*.coffee'],
+        dest: 'app/tests/',
+        options: {
+            bare: false
+        }
+      }
+    },
+
+    concat: {
+      app: {
+        src: [
+          '<banner:meta.banner>',
+          'app/js/model.js',
+          'app/js/collection.js',
+          'app/js/view.js',
+          'app/js/layout.js',
+          'app/js/data-manager.js'
+        ],
+        dest: 'app/app.js'
+      }
+    },
+
+    min: {
+      dist: {
+        src: [
+            '<banner:meta.banner>',
+            '<config:concat.app.dest>'
+          ],
+          dest: 'app/app.min.js'
+        }
+      },
+
+    lint: {
+      files: [
+        'app/js/*.js'
+        // 'app/tests/*.js' // Из-за хитрости тестов было много хинтов, решили отключить
+      ]
+    },
+
+    watch: {
+      files: [
+        '<config:coffee.app.src>',
+        '<config:coffee.tests.src>'
+      ],
+      tasks: 'rebuild'
+    }
+
+  });
+
+  // Default task.
+  registerTask('default', 'coffee concat min');
+  registerTask('rebuild', 'coffee concat min')
+
+};
